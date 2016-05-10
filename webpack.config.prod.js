e3c6ef5ceb2b2d6f810fd8d0bb86733735d463c1
kerwinp <piaoyuankui@gmail.com>
2016-05-10 10:29:19 +0800

    Build production files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,37 @@
 var webpack = require('webpack');
-var config = require('./webpack.config.dev.js');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var devConfig = require('./webpack.config.dev.js');
+var _ = require('lodash');
 
-config.plugins = [
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      warnings: false,
-    }
-  }),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  }),
+function generateConfig(module) {
+  var config = _.clone(devConfig, true);
+  config.entry = './' + module + '/index.js';
+  config.output = {
+    path: __dirname + '/dist/' + module,
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+  config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ];
+
+  return config;
+}
+
+module.exports = [
+  generateConfig('boss'),
+  generateConfig('console'),
 ];
-
-module.exports = config;
