e75c658792ea4ea205ec620558640773ae4375fd
kerwin <piaoyuankui@gmail.com>
2016-06-28 11:47:25 +0800

    Support download log

--- console/pages/InstanceTabMonitor.jsx
+++ console/pages/InstanceTabMonitor.jsx
@@ -3,15 +3,20 @@ import Page, { attach } from '../../shared/pages/Page';
 import * as Actions from '../redux/actions';
 
 class C extends Page {
+  componentDidMount() {
+    const { t, dispatch, region } = this.props;
+    dispatch(Actions.setHeader(t('instanceManage'), `/${region.regionId}/instances`));
+
+    this.refresh();
+  }
+
+  refresh() {
+  }
+
   render() {
     return (
       <div />
     );
-  }
-
-  componentDidMount() {
-    const { t, dispatch, region } = this.props;
-    dispatch(Actions.setHeader(t('instanceManage'), `/${region.regionId}/instances`));
   }
 }
 
--- console/pages/InstanceTabOutput.jsx
+++ console/pages/InstanceTabOutput.jsx
@@ -5,28 +5,78 @@ import * as InstanceActions from '../redux/actions.instance';
 
 class C extends Page {
 
+  constructor(props) {
+    super(props);
+
+    this.refresh = this.refresh.bind(this);
+    this.download = this.download.bind(this);
+    this.state = {
+      outputDownloadUrl: undefined,
+    };
+  }
+
   componentDidMount() {
     const { t, dispatch, region } = this.props;
     dispatch(Actions.setHeader(t('instanceManage'), `/${region.regionId}/instances`));
 
-    this.setInterval(() => {
-      this.refresh();
-    }, 2000);
-
     this.refresh();
   }
 
-  refresh() {
+  download(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    this.setState({
+      outputDownloadUrl: this.props.context.url,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        outputDownloadUrl: undefined,
+      });
+    }, 100);
+  }
+
+  refresh(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
     const { routerKey, dispatch, region, params } = this.props;
-    dispatch(InstanceActions.requestInstanceOutput(routerKey, region.regionId, params.instanceId));
+    dispatch(InstanceActions.requestInstanceOutput(routerKey, region.regionId, params.instanceId))
+    .then(() => {
+      dispatch(Actions.extendContext({ loading: false }, routerKey));
+    });
+
+    dispatch(Actions.extendContext({ loading: true }, routerKey));
   }
 
   render() {
+    const { t } = this.props;
+    const { outputDownloadUrl } = this.state;
     return (
       <div>
-        {this.props.context.output && <pre className="console_output">
-          {this.props.context.output}
-        </pre>}
+        <div className="gray-content-block second-block">
+          <div className="filter-item inline">
+            <a className="btn btn-default" onClick={this.refresh}>
+              <i className={`fa fa-refresh ${this.props.context.loading ? 'fa-spin' : ''}`}></i>
+            </a>
+          </div>
+          {this.props.context.url && <div className="filter-item inline pull-right">
+            <a className="btn btn-success" onClick={this.download}>
+              <i className="fa fa-download"></i>&nbsp;{t('download')}
+            </a>
+          </div>}
+          <iframe className="hide" src={outputDownloadUrl} />
+        </div>
+        {this.props.context.output && (
+          <div>
+            <pre className="console_output">
+              {this.props.context.output}
+            </pre>
+          </div>
+        )}
       </div>
     );
   }
