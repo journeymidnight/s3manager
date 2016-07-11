cc88bbc15f804951da88a640c831d4cf0f7889ec
kerwin <piaoyuankui@gmail.com>
2016-07-11 11:11:07 +0800

    Change LeStack -> LeCloud

--- shared/__tests__/i18n.spec.js
+++ shared/__tests__/i18n.spec.js
@@ -3,6 +3,6 @@ import i18n from '../i18n';
 
 describe('i18n', () => {
   it('#appName', () => {
-    expect(i18n.t('appName')).toEqual('LeStack');
+    expect(i18n.t('appName')).toEqual('LeCloud');
   });
 });
