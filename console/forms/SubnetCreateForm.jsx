e4a74dc69750e79e004f424999f02933b00456cf
jiangfei <jiangfei@letv.com>
2016-07-22 17:06:48 +0800

    fix bug:subnetCreateModal could not close

--- console/forms/SubnetCreateForm.jsx
+++ console/forms/SubnetCreateForm.jsx
@@ -9,28 +9,24 @@ const F = (props) => {
     handleSubmit,
     submitting,
     submitFailed,
-    resetForm,
     t,
     invalid,
   } = props;
   return (
     <form className="form-horizontal" onSubmit={handleSubmit}>
-
-      <div className={submitFailed && cidr.error ? 'form-group has-error' : 'form-group'}>
-        <label className="control-label" >{t('cidr')}</label>
-        <div className="col-sm-10">
-          <input type="text" className="form-control" {...cidr} />
-          {submitFailed && cidr.error && <div className="text-danger"><small>{cidr.error}</small></div>}
+      <div className="modal-body">
+        <div className={submitFailed && cidr.error ? 'form-group has-error' : 'form-group'}>
+          <label className="control-label" >{t('cidr')}</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" {...cidr} />
+            {submitFailed && cidr.error && <div className="text-danger"><small>{cidr.error}</small></div>}
+          </div>
         </div>
       </div>
-
-      <div className="form-actions">
+      <div className="modal-footer">
+        <button type="button" className="btn btn-default" data-dismiss="modal">{t('closeModal')}</button>
         <button type="submit" className="btn btn-save" disabled={submitting || invalid}>
           {submitting ? <i className="fa fa-spin fa-spinner" /> : <i />} {t('create')}
-        </button>
-        &nbsp;
-        <button type="button" className="btn btn-cancel" disabled={submitting} onClick={resetForm}>
-          {t('reset')}
         </button>
       </div>
     </form>
@@ -44,7 +40,6 @@ F.propTypes = {
   handleSubmit: React.PropTypes.func.isRequired,
   submitting: React.PropTypes.bool.isRequired,
   submitFailed: React.PropTypes.bool.isRequired,
-  resetForm: React.PropTypes.func.isRequired,
   t: React.PropTypes.any,
 };
 
