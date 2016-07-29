de333312f8f8fa7100af3f7dcfb6e2d41890888a
kerwin <piaoyuankui@gmail.com>
2016-07-29 13:50:51 +0800

    Fix mixpanel issue

--- shared/analytics.js
+++ shared/analytics.js
@@ -1,7 +1,6 @@
-import Mixpanel from 'mixpanel-browser';
+import mixpanel from 'mixpanel-browser';
 
-const mixpanel = Mixpanel.init('cc1f30308ba75740c13580c25937577d');
-
+mixpanel.init('cc1f30308ba75740c13580c25937577d');
 mixpanel.track('Visited', {
   source: undefined,
 });
