b252f0f834417b36ccf36d47af8d37a0a938115c
jiangfei <jiangfei@letv.com>
2016-08-02 16:12:52 +0800

    add initial values in eipForm and volumeForm

--- console/forms/VolumeForm.jsx
+++ console/forms/VolumeForm.jsx
@@ -3,81 +3,94 @@ import { translate } from 'react-i18next';
 import { reduxForm } from 'redux-form';
 import * as Validations from '../../shared/utils/validations';
 
-const F = (props) => {
-  const { fields:
-    { name, snapshotId, size, count },
-    handleSubmit,
-    submitting,
-    submitFailed,
-    resetForm,
-    t,
-  } = props;
-  const selectedSnapshot = snapshotId.value ? props.availableSnapshots.filter((snapshot) => {
-    return snapshot.snapshotId === snapshotId.value;
-  })[0] : null;
-  return (
-    <form className="form-horizontal" onSubmit={handleSubmit}>
+class F extends React.Component {
 
-      <div className={submitFailed && name.error ? 'form-group has-error' : 'form-group'}>
-        <label className="control-label" >{t('name')}</label>
-        <div className="col-sm-10">
-          <input type="text" className="form-control" {...name} />
-          {submitFailed && name.error && <div className="text-danger"><small>{name.error}</small></div>}
+  componentDidMount() {
+    const initialValues = {
+      name: '',
+      snapshotId: undefined,
+      size: 10,
+      count: 1,
+    };
+    this.props.initializeForm(initialValues);
+  }
+  render() {
+    const { fields:
+      { name, snapshotId, size, count },
+      handleSubmit,
+      submitting,
+      submitFailed,
+      resetForm,
+      t,
+    } = this.props;
+    const selectedSnapshot = snapshotId.value ? this.props.availableSnapshots.filter((snapshot) => {
+      return snapshot.snapshotId === snapshotId.value;
+    })[0] : null;
+    return (
+      <form className="form-horizontal" onSubmit={handleSubmit}>
+
+        <div className={submitFailed && name.error ? 'form-group has-error' : 'form-group'}>
+          <label className="control-label" >{t('name')}</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" {...name} />
+            {submitFailed && name.error && <div className="text-danger"><small>{name.error}</small></div>}
+          </div>
         </div>
-      </div>
 
-      {props.availableSnapshots.length > 0 && <div className={submitFailed && snapshotId.error ? 'form-group has-error' : 'form-group'}>
-        <label className="control-label" >{t('pageVolume.snapshot')}</label>
-        <div className="col-sm-10">
-          <select className="form-control" {...snapshotId}>
-            <option value="">{t('pageVolume.selectSnapshotPlease')}</option>
-            {props.availableSnapshots.map((snapshot) => {
-              return <option key={snapshot.snapshotId} value={snapshot.snapshotId}>{snapshot.name}</option>;
-            })}
-          </select>
-          {submitFailed && snapshotId.error && <div className="text-danger"><small>{snapshotId.error}</small></div>}
+        {this.props.availableSnapshots.length > 0 && <div className={submitFailed && snapshotId.error ? 'form-group has-error' : 'form-group'}>
+          <label className="control-label" >{t('pageVolume.snapshot')}</label>
+          <div className="col-sm-10">
+            <select className="form-control" {...snapshotId}>
+              <option value="">{t('pageVolume.selectSnapshotPlease')}</option>
+              {this.props.availableSnapshots.map((snapshot) => {
+                return <option key={snapshot.snapshotId} value={snapshot.snapshotId}>{snapshot.name}</option>;
+              })}
+            </select>
+            {submitFailed && snapshotId.error && <div className="text-danger"><small>{snapshotId.error}</small></div>}
+          </div>
+        </div>}
+
+        {!selectedSnapshot && <div className={submitFailed && size.error ? 'form-group has-error' : 'form-group'}>
+          <label className="control-label" >{t('size')}</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" {...size} />
+            {submitFailed && size.error && <div className="text-danger"><small>{size.error}</small></div>}
+          </div>
+        </div>}
+
+        {selectedSnapshot && <div className="form-group">
+          <label className="control-label" >{t('size')}</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" value={selectedSnapshot.size} disabled="disabled" />
+          </div>
+        </div>}
+
+        <div className={submitFailed && count.error ? 'form-group has-error' : 'form-group'}>
+          <label className="control-label" >{t('count')}</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" {...count} />
+            {submitFailed && count.error && <div className="text-danger"><small>{count.error}</small></div>}
+          </div>
         </div>
-      </div>}
 
-      {!selectedSnapshot && <div className={submitFailed && size.error ? 'form-group has-error' : 'form-group'}>
-        <label className="control-label" >{t('size')}</label>
-        <div className="col-sm-10">
-          <input type="text" className="form-control" {...size} />
-          {submitFailed && size.error && <div className="text-danger"><small>{size.error}</small></div>}
+        <div className="form-actions">
+          <button type="submit" className="btn btn-save" disabled={submitting}>
+            {submitting ? <i className="fa fa-spin fa-spinner" /> : <i />} {t('update')}
+          </button>
+          &nbsp;
+          <button type="button" className="btn btn-cancel" disabled={submitting} onClick={resetForm}>
+            {t('reset')}
+          </button>
         </div>
-      </div>}
-
-      {selectedSnapshot && <div className="form-group">
-        <label className="control-label" >{t('size')}</label>
-        <div className="col-sm-10">
-          <input type="text" className="form-control" value={selectedSnapshot.size} disabled="disabled" />
-        </div>
-      </div>}
-
-      <div className={submitFailed && count.error ? 'form-group has-error' : 'form-group'}>
-        <label className="control-label" >{t('count')}</label>
-        <div className="col-sm-10">
-          <input type="text" className="form-control" {...count} />
-          {submitFailed && count.error && <div className="text-danger"><small>{count.error}</small></div>}
-        </div>
-      </div>
-
-      <div className="form-actions">
-        <button type="submit" className="btn btn-save" disabled={submitting}>
-          {submitting ? <i className="fa fa-spin fa-spinner" /> : <i />} {t('update')}
-        </button>
-        &nbsp;
-        <button type="button" className="btn btn-cancel" disabled={submitting} onClick={resetForm}>
-          {t('reset')}
-        </button>
-      </div>
-    </form>
-  );
-};
+      </form>
+    );
+  }
+}
 
 F.propTypes = {
   fields: React.PropTypes.object.isRequired,
   error: React.PropTypes.string,
+  initializeForm: React.PropTypes.func.isRequired,
   handleSubmit: React.PropTypes.func.isRequired,
   submitting: React.PropTypes.bool.isRequired,
   submitFailed: React.PropTypes.bool.isRequired,
