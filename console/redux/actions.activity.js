cda411a02be76c9f6e9bb429528f5e19e9a2c539
jiangfei <jiangfei@letv.com>
2016-08-11 13:41:19 +0800

    adjust operation api

--- console/redux/actions.activity.js
+++ console/redux/actions.activity.js
@@ -19,3 +19,21 @@ export function requestDescribeJobs(routerKey, regionId, filters) {
   };
 }
 
+export function requestDescribeOperations(routerKey, regionId, filters) {
+  return dispatch => {
+    return IaaS
+      .describeOperations(regionId, filters)
+      .promise
+      .then((payload) => {
+        dispatch(extendContext(Object.assign(payload, {
+          currentPage: parseInt(payload.offset / payload.limit, 10) + 1,
+          size: payload.limit,
+          totalPage: parseInt((payload.total - 1) / payload.limit, 10) + 1,
+        })));
+      })
+      .catch((error) => {
+        dispatch(notifyAlert(error.message));
+      });
+  };
+}
+
