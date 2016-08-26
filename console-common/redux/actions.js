87396f29598173aa7fb09d51ee698aaa87e444c4
kerwin <piaoyuankui@gmail.com>
2016-08-26 16:42:59 +0800

    Fix unit test issue

--- console-common/redux/actions.js
+++ console-common/redux/actions.js
@@ -66,7 +66,7 @@ export function authLogout() {
 export function requestLogout() {
   return dispatch => {
     dispatch(authLogout());
-    dispatch(push('/login'));
+    dispatch(push('/'));
   };
 }
 
