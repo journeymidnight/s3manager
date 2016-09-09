d916030d6968166a72f23633c55cf0b5060a6a52
jiangfei <jiangfei@letv.com>
2016-09-09 16:16:19 +0800

    optimze server error msg

--- lcs/services/ErrorService.js
+++ lcs/services/ErrorService.js
@@ -2,8 +2,8 @@ import i18n from '../../shared/i18n';
 
 export const serverErrorHandler = (error) => {
   if (error.retCode === 4113) {
-    error.displayMsg = i18n.t('errorMsg.4113');
+    const resource = /quota\[(\w+)\]/.exec(error.message)[1];
+    error.displayMsg = i18n.t('errorMsg.4113').replace('{resource}', i18n.t(`formQuotaForm.${resource}`));
     return;
   }
 };
-
