9bd599959048a2e24224c4a43f75c760e3a9e0ee
jiangfei <jiangfei@letv.com>
2016-09-13 15:49:40 +0800

    fix lint error

--- shared/components/SearchBox.jsx
+++ shared/components/SearchBox.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 class SearchBox extends React.Component {
-render() {
-        return (
-            <div className="search-box">
-                <input type="search" ref="search" placeholder={this.props.placeholder} onKeyPress={this.props.onEnterPress} />
-                <button onClick={this.props.onButtonClick}><i className="fa fa-search" aria-hidden="true"></i></button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="search-box">
+        <input type="search" ref="search" placeholder={this.props.placeholder} onKeyPress={this.props.onEnterPress} />
+        <button onClick={this.props.onButtonClick}><i className="fa fa-search" aria-hidden="true"></i></button>
+      </div>
+  );
+  }
 }
 
 SearchBox.propTypes = {
-    placeholder: React.PropTypes.string,
-    t: React.PropTypes.func,
-    onEnterPress: React.PropTypes.func,
-    onButtonClick: React.PropTypes.func,
+  placeholder: React.PropTypes.string,
+  t: React.PropTypes.func,
+  onEnterPress: React.PropTypes.func,
+  onButtonClick: React.PropTypes.func,
 };
 
 export default SearchBox;
