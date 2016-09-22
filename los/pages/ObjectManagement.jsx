5f13d1cd827015bb4c910e34b8b52d5aaedc2e41
王丁 <wangding@le.com>
2016-09-22 18:01:21 +0800

    Implement uploading objects for object management page

--- los/pages/ObjectManagement.jsx
+++ los/pages/ObjectManagement.jsx
@@ -2,10 +2,11 @@ import _ from 'lodash';
 import moment from 'moment';
 import AWS from 'aws-sdk';
 import React from 'react';
+import update from 'react-addons-update';
 import { Link } from 'react-router';
 import { attach } from '../../shared/pages/Page';
-import { confirmModal } from '../../shared/components/Modal';
 import { buttonForm } from '../../shared/forms/ButtonForm';
+import Modal, { confirmModal } from '../../shared/components/Modal';
 import TablePageStatic from '../../shared/pages/TablePageStatic';
 import * as Actions from '../../console-common/redux/actions';
 import * as ObjectActions from '../redux/actions.object';
@@ -15,8 +16,22 @@ class C extends TablePageStatic {
 
   constructor(props) {
     super(props);
+    const { t } = this.props;
     this.state = {
       s3: null,
+      status: {
+        uploading: t('uploadModal.uploading'),
+        uploaded: t('uploadModal.uploaded'),
+        paused: t('uploadModal.paused'),
+        canceled: t('uploadModal.canceled'),
+        failed: t('uploadModal.failed'),
+      },
+      action: {
+        pause: t('uploadModal.pause'),
+        continue: t('uploadModal.continue'),
+        cancel: t('uploadModal.cancel'),
+        retry: t('uploadModal.retry'),
+      },
     };
 
     this.refresh = this.refresh.bind(this);
@@ -24,6 +39,8 @@ class C extends TablePageStatic {
     this.onDelete = this.onDelete.bind(this);
     this.onSearchKeyPress = this.onSearchKeyPress.bind(this);
     this.formatBytes = this.formatBytes.bind(this);
+    this.onFileUpload = this.onFileUpload.bind(this);
+    this.uploadOneObject = this.uploadOneObject.bind(this);
   }
 
   initialize(routerKey) {
@@ -39,7 +56,7 @@ class C extends TablePageStatic {
         const s3 = new AWS.S3();
         this.setState({
           s3,
-        }, () => (this.initTable(routerKey, {})));
+        }, () => this.initTable(routerKey, {}));
       });
   }
 
@@ -84,6 +101,81 @@ class C extends TablePageStatic {
     else if (bytes < 1024 * 1024 * 1024) return ((bytes / 1024 / 1024).toFixed(1) + 'MB');
     else if (bytes < 1024 * 1024 * 1024 * 1024) return ((bytes / 1024 / 1024 / 1024).toFixed(1) + 'GB');
     return ((bytes / 1024 / 1024 / 1024 / 1024).toFixed(1) + 'TB');
+  }
+
+  onFileUpload() {
+    const files = this.refs.fileUploader.files;
+
+    const uploadingFileList = {};
+    for (let i = 0, len = files.length; i < len; i++) {
+      uploadingFileList[i] = {
+        name: files[i].name,
+        size: files[i].size,
+        percent: 0,
+        status: 'uploading',
+        actions: [
+          'pause',
+          'cancel',
+        ],
+      };
+    }
+
+    this.setState({
+      uploadingFileList,
+    }, () => {
+      this.refs.uploadModal.show();
+      for (let i = 0, len = files.length; i < len; i++) {
+        this.uploadOneObject(files[i], i);
+      }
+    });
+  }
+
+  uploadOneObject(file, index) {
+    const fileName = file.name;
+    const s3Uploader = this.state.s3.upload(
+      {
+        Bucket: this.props.global.bucketName,
+        Key: fileName,
+        Body: file,
+      },
+      {
+        partSize: 5 * 1024 * 1024,
+        queueSize: 10,
+        leavePartsOnError: true,
+      },
+      (error, data) => {
+        if (error) {
+          if (error.code !== 'RequestAbortedError') {
+            const newUploadingFileList = update(this.state.uploadingFileList, {
+              [index]: { status: { $set: 'failed' } },
+            });
+            this.setState({
+              uploadingFileList: newUploadingFileList,
+            });
+          }
+        } else {
+          let newUploadingFileList = update(this.state.uploadingFileList, {
+            [index]: { status: { $set: 'uploaded' } },
+          });
+          newUploadingFileList = update(newUploadingFileList, {
+            [index]: { percent: { $set: 0 } },
+          });
+          this.setState({
+            uploadingFileList: newUploadingFileList,
+          }, () => this.onRefresh({}, false)());
+        }
+      }
+    );
+
+    s3Uploader.on('httpUploadProgress', (progress) => {
+      const percent = 100 * progress.loaded / progress.total;
+      const newUploadingFileList = update(this.state.uploadingFileList, {
+        [index]: { percent: { $set: percent } },
+      });
+      this.setState({
+        uploadingFileList: newUploadingFileList,
+      });
+    });
   }
 
   renderTable() {
@@ -132,14 +224,80 @@ class C extends TablePageStatic {
           <span>{t('bucket')}&nbsp;<i>{params.bucketName}</i></span>
         </div>
         <div className="nav-controls">
-          <Link className="btn btn-new" to={`${servicePath}/objects/create`}>
-            <i className="fa fa-upload" />&nbsp;{t('uploadFile')}
-          </Link>
+          <input
+            className="btn btn-new"
+            type="file"
+            id="fileUploader"
+            ref="fileUploader"
+            multiple="true"
+            style={{ display: 'none' }}
+            onChange={this.onFileUpload}
+          />
+          <label htmlFor="fileUploader" style={{ marginRight: 10 }}>
+            <div className="btn btn-new">
+              <i className="fa fa-upload" />&nbsp;{t('uploadFile')}
+            </div>
+          </label>
 
           <Link className="btn btn-new" to={`${servicePath}/objects/create`}>
             <i className="fa fa-plus" />&nbsp;{t('createFolder')}
           </Link>
         </div>
+        <Modal title={t('uploadModal.uploadingStatus')} ref="uploadModal" >
+          <div>
+            {this.state.uploadingFileList && <div className="content-wrapper">
+              <div
+                className="container-fluid container-limited"
+                style={{
+                  minHeight: 0,
+                }}
+              >
+                <div className="content">
+                  <div className="clearfix">
+                    <div className="table-holder">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th width="400">{t('fileName')}</th>
+                            <th width="200">{t('size')}</th>
+                            <th width="200">{t('status')}</th>
+                            <th width="200">{t('action')}</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        {Object.keys(this.state.uploadingFileList).map((key) => {
+                          const file = this.state.uploadingFileList[key];
+                          return (
+                            <tr key={file.name}>
+                              <td style={{ position: 'relative' }}>
+                                <div>{file.name}</div>
+                                <div
+                                  style={{
+                                    width: file.percent + '%',
+                                    position: 'absolute',
+                                    height: '100%',
+                                    backgroundColor: '#0e90d2',
+                                    top: 0,
+                                    left: 0,
+                                    opacity: 0.5,
+                                  }}
+                                ></div>
+                              </td>
+                              <td>{this.formatBytes(file.size)}</td>
+                              <td>{this.state.status[file.status]}</td>
+                              <td>{file.actions.map((action) => this.state.action[action])}</td>
+                            </tr>
+                          );
+                        })}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>}
+          </div>
+        </Modal>
       </div>
     );
   }
